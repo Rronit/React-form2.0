6bec67caef0e1a7ca6d51bff7df90890753fa9bf
Rronit <rronit2015@gmail.com>
2022-09-25 00:05:16 +0530

    feat: midway

--- src/Components/UI/Card.tsx
+++ src/Components/UI/Card.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 import style from "./Card.module.css";
 
 const Card = (props: any) => {
+  useEffect(() => {
+    console.log("ddddddddddd111111");
+  });
+  console.log("dddddd");
+  useLayoutEffect(() => {
+    console.log("sssssssssssss");
+  });
+  useEffect(() => {
+    console.log("dd222222222ddd");
+  });
   return (
     <div className={`${props.className} ${style.card}`}>{props.children}</div>
   );
